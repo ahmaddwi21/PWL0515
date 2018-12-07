a1b16b63e178c4d320bd056e07f3a33f1d3e5b38
ahmaddwi21 <ahmad.sulistiyo@students.amikom.ac.id>
2018-12-07 10:38:03 +0700

    update 2

--- updateadmin.php
+++ updateadmin.php
@@ -1,5 +1,21 @@
 <?php
 require_once('DataKoneksi.php');
-$query = mysqli_query($con, "select * from admin where id_admin='".$_GET['id_admin']."' ");
-    while($record= )
-?>
+$query1 = mysqli_query($con, "select * from admin where id_admin='".$_GET['id_admin']."' ");
+    while($record = mysqli_fetch_array($query1)){
+        ?>
+<html>
+<head>
+
+</head>
+
+<body>
+    <h3>Update Admin</h3>
+    <form action="" method="post">
+        <table width="200" border="0">
+            <tr>
+                
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
